8a7a757e41d4cee6cf00f913aa9372deee5fc4a1
victormc13 <viktorsb14@gmail.com>
2023-02-06 17:50:25 -0500

    Add function of the menu button

---
+++ script.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector(".menu-mobile") 
+
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    menu.style.width = "100%";
+});
+
+document.querySelector(".close-btn").addEventListener("click", () => {
+    menu.style.width = "0";
+});
