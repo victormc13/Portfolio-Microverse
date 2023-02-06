b07e1eb776e404ab2c4b76273a8d31da3b708ce6
victormc13 <viktorsb14@gmail.com>
2023-02-06 18:22:56 -0500

    Add and set up linters properly

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const menu = document.querySelector(".menu-mobile") 
+const menu = document.querySelector('.menu-mobile');
 
-document.querySelector(".menu-btn").addEventListener("click", () => {
-    menu.style.width = "100%";
+document.querySelector('.menu-btn').addEventListener('click', () => {
+  menu.style.width = '100%';
 });
 
-document.querySelector(".close-btn").addEventListener("click", () => {
-    menu.style.width = "0";
+document.querySelector('.close-btn').addEventListener('click', () => {
+  menu.style.width = '0';
 });
